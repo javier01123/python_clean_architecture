604bac94d3dcd31e719c3053a5c8ad4f8ab6f3b0
Javier Andrade <javier.andrade@ventamovil.com.mx>
2020-11-29 17:23:38 -0700

    definicion de la prueba de integracion de crear empresa

--- pos/tests/integration/test_create_company.py
+++ pos/tests/integration/test_create_company.py
@@ -1,11 +1,14 @@
 # pylint: disable=broad-except
 import pytest
-from pos.domain import models
-from pos.service_layer import unit_of_work, services
+from pos.domain import models, value_objects
+from pos.service_layer import services
+from pos.adapters import orm
+ 
+orm.start_mappers()
 
- 
- 
-# def test_create_company(session_factory): 
-#     uow = unit_of_work.SqlAlchemyUnitOfWork(session_factory)
-#     services.create_company('fiscal test name','comercial test name', 'XAXX010101000', uow)
-#     assert
+def test_create_company(): 
+    uow =  services.unit_of_work.SqlAlchemyUnitOfWork()
+    services.create_company('fiscal test name','comercial test name', 'XAXX010101000', uow)
+    company_created = uow.companies.get(0)
+    print(company_created.__dict__)
+    assert company_created.rfc == value_objects.Rfc('XAXX010101000')
